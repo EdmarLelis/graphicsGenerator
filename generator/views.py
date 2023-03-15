f06f865446c884f009a07b8217c291af6653a0f5
Edmar Lelis Loureço Júnior <120860142+EdmarLelis@users.noreply.github.com>
2023-03-15 19:40:55 -0300

    clock

--- generator/views.py
+++ generator/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import time
+from datetime import datetime
 
 
 # Create your views here.
 
 def selector(request):
-    time = time.now()
+    time = datetime.now()
     print(time)
     return render(request, 'generator.html')
 
